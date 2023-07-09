9bc51b1cc0dcaeb4b7760d1dc6659b1db4ed1625
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2023-07-08 21:52:55 -0500

    - Week 2 Day 3 4ever-clj problems

--- solutions-4clojure/src/solutions_4clojure/solutions.clj
+++ solutions-4clojure/src/solutions_4clojure/solutions.clj
@@ -456,11 +456,68 @@
 
 ;; 107, 90
 
+;; #107 - Simple closures
+;;
+;; Lexical scope and first-class functions are two of the most basic
+;; building blocks of a functional language like Clojure. When you combine
+;; the two together, you get something very powerful called lexical closures.
+;; With these, you can exercise a great deal of control over the lifetime of
+;; your local bindings, saving their values for use later, long after the code
+;; you're running now has finished. It can be hard to follow in the abstract, so
+;; let's build a simple closure.
+;;
+;; Given a positive integer n, return a function (f x)
+;; which computes x^n.
+;;
+;; Observe that the effect of this is to preserve the value of n
+;; for use outside the scope in which it is defined.
+
+(defn pow [exponent]
+  (fn [base]
+    (reduce * (repeat exponent base))))
+
+(= 256 ((pow 2) 16), ((pow 8) 2))
+(= [1 8 27 64] (map (pow 3) [1 2 3 4]))
+(= [1 2 4 8 16] (map #((pow %) 2) [0 1 2 3 4]))
+
+;; #90 - Cartesian Product
+;;
+;; Write a func which calcs the Cartesian product of two sets
+;;
+;; Uses List Comprehension with `for`
+(defn cartesian-product [set1 set2]
+  (set
+    (for [s1 set1
+          s2 set2]
+      (vector s1 s2))))
+
+(= (cartesian-product #{"ace" "king" "queen"} #{"♠" "♥" "♦" "♣"})
+   #{["ace"   "♠"] ["ace"   "♥"] ["ace"   "♦"] ["ace"   "♣"]
+     ["king"  "♠"] ["king"  "♥"] ["king"  "♦"] ["king"  "♣"]
+     ["queen" "♠"] ["queen" "♥"] ["queen" "♦"] ["queen" "♣"]})
+
+(= (cartesian-product #{1 2 3} #{4 5})
+   #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})
+
+(= 300 (count (cartesian-product (into #{} (range 10))
+                  (into #{} (range 30)))))
+
 ;; ====================
 ;; ~~~ Week 2 Day 4 ~~~
 ;; ====================
 
 ;; 88, 100
+
+;; #88 - Symmetric Difference
+;;
+;; Write a func which returns the symm diff of 2 sets
+;; The symm diff is the set of items belonging to ONE but NOT BOTH of the two sets.
+
+
+;; #100 - Least Common Multiple
+;;
+;; Write a func which cals the Least Common Multiple.
+;; Your func should accept a variable # of pos ints or ratios.
 
 ;; ====================
 ;; ~~~ Week 2 Day 5 ~~~
